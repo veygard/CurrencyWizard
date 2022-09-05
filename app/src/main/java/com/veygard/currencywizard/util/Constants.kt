d8401cb9af2520e81173739d1f79a2e2722064fa
Boris_Sadakov <boris.sadakov@sfera.zone>
2022-09-05 09:53:14 +0300

    new Api key.

--- app/src/main/java/com/veygard/currencywizard/util/Constants.kt
+++ app/src/main/java/com/veygard/currencywizard/util/Constants.kt
@@ -2,5 +2,5 @@ package com.veygard.currencywizard.util
 
 object Constants {
     const val BASE_URL = "https://api.fastforex.io"
-    const val API_KEY = "bb1a2810ce-f2af08afa1-rhh59v"
+    const val API_KEY = "6f3316734c-895da32289-rhq5qz"
 }
